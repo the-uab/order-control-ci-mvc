f2200f8766a90de3b5e172fbfc0353ba5e2b39b4
SergioCusi <samichoanghelo@gmail.com>
2015-06-18 15:00:31 +0000

    Responsiv roles

---
+++ application/views/navbar - login.php
@@ -0,0 +1,41 @@
+
+<!--	<p><?php //echo $user;?></p>
+			<div class="span12">
+				
+				 <ul class="nav nav-tabs">
+					<li><a href="<?php //echo base_url().'Inicio' ?>">Inicio</a></li>
+					<li><a href="<?php //echo base_url().'Usuario' ?>">Usuario</a></li>
+					<li><a href="<?php  //echo base_url().'Roles' ?>">Roles</a></li>
+					<li><a href="<?php  //echo base_url()?>login/logout">Cerrar Sesión</a></li>
+				</ul>
+			</div>
+		</di
+-->
+
+<header class="site-header">
+				<div class="top-header">
+					<div class="container">
+						
+						
+						<nav class="member-navigation pull-right">
+							
+						</nav> <!-- .member-navigation -->
+					</div> <!-- .container -->
+				</div> <!-- .top-header -->
+
+				<div class="bottom-header">
+					<div class="container">
+						<a href="<?php echo base_url().'Inicio' ?>" class="branding pull-left">
+							<img src="img/logo-icon.png" alt="Site title" class="logo-icon">
+							<h1 class="site-title">Project <span>SUAB</span></h1> 
+							<h2 class="site-description"></h2>
+						</a> <!-- #branding -->
+					
+					</div> <!-- .container -->
+				</div> <!-- .bottom-header -->
+			</header> <!-- .site-header -->
+			<main class="content">
+
+				<div class="fullwidth-block feature-section">
+					<div class="container">
+						<div class="row">
